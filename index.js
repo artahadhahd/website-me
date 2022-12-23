115148824ad17ed3ce348ce837ee7f084848e641
artahadhahd <116200657+artahadhahd@users.noreply.github.com>
2022-12-23 23:52:36 +0000

    menu and bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var clicked = 0;
 function theme() {
+    const changeable = document.querySelectorAll(".change");
     ++clicked;
     if (clicked % 2 != 0)
     {
@@ -7,6 +8,9 @@ function theme() {
         document.getElementById("background-blue2").style.backgroundColor = "#BF2904";
         document.getElementById("themebtn").style.backgroundColor = "#BF2904";
         document.getElementById("themebtn").style.borderColor = "#BF2904";
+        [...changeable].forEach((elem) => {
+            elem.style.backgroundColor = "#BF2904";
+        });
     }
     else 
     {
@@ -14,6 +18,8 @@ function theme() {
         document.getElementById("background-blue2").style.backgroundColor = "#2388da";
         document.getElementById("themebtn").style.backgroundColor = "#2388da";
         document.getElementById("themebtn").style.borderColor = "#2388da";
-        // document.querySelectorAll("#change").style.backgroundColor = "#2388da"
+        [...changeable].forEach((elem) => {
+            elem.style.backgroundColor = "#2388da";
+        });
     }
 }
