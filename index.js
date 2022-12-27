9b53103ac969994509a14eeb3edc89044f1e9a67
artahadhahd <116200657+artahadhahd@users.noreply.github.com>
2022-12-27 13:07:38 +0000

    More animation + bug fixes

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 var count = 1;
-function animation() {
-    const doanimation = document.querySelectorAll(".animation");
-    const showtext = document.querySelectorAll("#showtext");
-    count++;
-    if (count % 5 == 0){
-        [...doanimation].forEach((elem) => {
-            elem.style.rotate="-360deg";
+
+function funny() {
+    const find = document.querySelectorAll(".funny");
+    for (var i = 0; i <= count; i++){
+        [...find].forEach((element) => {
+            element.style.rotate = "360deg";
         });
     }
-    else if (count % 2 == 0) {
+}
+
+function animation() {
+    const doanimation = document.querySelectorAll(".animation");
+    count++;
+    if (count % 2 == 0){
         [...doanimation].forEach((elem) => {
-            elem.style.rotate="-20deg";
+            elem.innerHTML = `
+            <img src="./wave.png" style="height: 5cm; width: 5cm; position: absolute; margin-top: 7vw; margin-left: 10vw;"> 
+            <p style="color: white; position: absolute; margin-left: 37vw; margin-top: 20vw; font-size: 1cm; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">How are you?</p>
+            `;
+            [...doanimation].forEach((elem) => {
+                elem.style.rotate="-1deg";
+            });
         });
     }
     else {
         [...doanimation].forEach((elem) => {
-            elem.style.rotate="10deg";
+            elem.style.rotate="15deg";
         });
+        funny();
     }
-    [...showtext].forEach((element) => {
-        element.innerHTML = `
-        <p class="text">The W#5454<br>&nbsp;&nbsp;&nbsp;&nbsp;
-        age:&nbsp;14<br>&nbsp;&nbsp;&nbsp;
-        sex:&nbsp;male</p>
-        `;
-    });
-    console.log(count);
 }
