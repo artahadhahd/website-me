658d2bd3a1bcab60adc63e77d34069d6e5b3be64
artahadhahd <iamarta.azadegan@gmail.com>
2023-01-25 17:54:29 +0100

    improved code

--- Game/game.js
+++ Game/game.js
@@ -1,25 +1,21 @@
 head = document.getElementById("head");
+apple = document.getElementById("apple");
 
 function changePosition()
 {
-    apple = document.getElementById("apple");
     var height_maximum = 200;
     var width_maximum = 1000;
     var LEFT = Math.floor(Math.random() * (width_maximum - 10 + 1) + 10);
     var TOP = Math.floor(Math.random() * (height_maximum - 10 + 1) + 10);
     // The following code is written to keep the coordinates correct
     // Rule: x,y mod 10 should be 0.
-    if (LEFT % 10 != 0){
-        LEFT = LEFT - (LEFT % 10);
-    }
-    if (TOP % 10 != 0){
-        TOP = TOP - (TOP % 10);
-    }
+    LEFT -= LEFT % 10;
+    TOP -= TOP % 10;
     console.log(LEFT, TOP);
     // -2 because for some reason the head cannot be aligned with the apple.
     apple.style.left = (LEFT-2).toString() + "px";
     apple.style.top = TOP.toString() + "px";
-    return [LEFT, TOP]; // ^?
+    return [LEFT, TOP];
 }
 document.addEventListener('keydown', function(keyboard) {
     previous_xpos = head.getBoundingClientRect().left;
